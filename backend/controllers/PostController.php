b69501ac4d4b2342414361e578ad4e692167ea92
Prawee Wongsa <prawee@ezgroup.com>
2020-10-09 10:40:30 +0700

    how to extends user and access control class for more your logic

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -8,7 +8,7 @@ use backend\models\PostSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
+use common\components\AccessControl;
 
 /**
  * PostController implements the CRUD actions for Post model.
@@ -31,6 +31,10 @@ class PostController extends Controller
                     ],
                 ],
             ],*/
+            'access' => [
+                //'class' => 'common\components\AccessControl',
+                'class' => AccessControl::class
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -46,7 +50,7 @@ class PostController extends Controller
      */
     public function actionIndex()
     {
-        if (Yii::$app->user->can('post-list')) {
+        //if (Yii::$app->user->can('post-list')) {
             $searchModel = new PostSearch();
             $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
@@ -54,7 +58,7 @@ class PostController extends Controller
                 'searchModel' => $searchModel,
                 'dataProvider' => $dataProvider,
             ]);
-        }
+        //}
     }
 
     /**
---
+++ common/components/User.php
@@ -0,0 +1,19 @@
+<?php
+namespace common\components;
+
+class User extends \yii\web\User
+{
+    public $identityClass = '\common\models\User';
+
+    //logic
+    public function checkAccess($operation, $params = [], $allowCaching = true) {
+        // Always return true when SuperAdmin user
+        if (
+            \Yii::$app->user->id == 1
+            && \Yii::$app->user->status == 10
+        ) {
+            return true;
+        }
+        return parent::can($operation, $params, $allowCaching);
+    }
+}
